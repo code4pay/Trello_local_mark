a020ef06d68b4954b2ed009dd8192fe86f8ba897
Michael Mueller <michael.mueller@binary.com>
2019-03-21 11:38:28 +1100

    ensure it works when nothing is in local storage

--- default.js
+++ default.js
@@ -24,7 +24,7 @@ function addToChecked(e) {
     var parent_el = e.closest('div.list-card-details.js-card-details')
     var id = parent_el.querySelector(' span > span').innerText;
     // Already checked uncheck
-    if (checked.includes(id)) {
+    if (checked && checked.includes(id)) {
         checked = checked.filter(item => item !== id); 
 
     } else {
@@ -38,9 +38,10 @@ function addToChecked(e) {
 function updateCards() {
     var el = document.querySelector('#board > div:nth-child(6) > div > .list-cards').children ;
     checked = JSON.parse(localStorage.getItem('binary_checked'));
+    if (!checked) {checked = [];}
     for(child of el){
         var cardId = child.querySelector('div.list-card-details.js-card-details > span > span').innerText;
-        if (checked.includes(cardId)) {
+        if (checked && checked.includes(cardId)) {
             child.style.border = "medium dashed green"; 
         } else
         {
